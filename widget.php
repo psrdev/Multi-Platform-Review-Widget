9675888aa5c6b8985c0714968a2f49e74c744914
Pravin Singh Rana <dev.psrana@gmail.com>
2025-02-20 21:35:57 +0530

    Added target blank

--- widget.php
+++ widget.php
@@ -13,18 +13,18 @@ function mprw_add_widget()
         <div class="mprw-widget-button">
             <div class="mprw-widget-cta"><?php echo esc_html($options['cta_text'] ?? ''); ?></div>
             <ul class="mprw-widget-platforms">
-                <li><a href="<?php echo esc_url($options['platforms']['google']['link'] ?? '#'); ?>"><img
+                <li><a href="<?php echo esc_url($options['platforms']['google']['link'] ?? '#'); ?>" target="_blank"><img
                             src="<?php echo MPRW_PLUGIN_URL . "assets/google.svg" ?>" alt="Google" width="30px"
                             height="30px"></a></li>
-                <li><a href="<?php echo esc_url($options['platforms']['facebook']['link'] ?? '#'); ?>"><img
+                <li><a href="<?php echo esc_url($options['platforms']['facebook']['link'] ?? '#'); ?>" target="_blank"><img
                             src="<?php echo MPRW_PLUGIN_URL . "assets/facebook.svg" ?>" alt="Facebook" width="30px"
                             height="30px"></a></li>
-                <li><a href="<?php echo esc_url($options['platforms']['tripadvisor']['link'] ?? '#'); ?>"><img
-                            src="<?php echo MPRW_PLUGIN_URL . "assets/tripadvisor.svg" ?>" alt="Tripadvisor" width="30px"
-                            height="30px"></a></li>
-                <li><a href="<?php echo esc_url($options['platforms']['trustpilot']['link'] ?? '#'); ?>"><img
-                            src="<?php echo MPRW_PLUGIN_URL . "assets/trustpilot.svg" ?>" alt="Trustpilot" width="30px"
-                            height="30px"></a></li>
+                <li><a href="<?php echo esc_url($options['platforms']['tripadvisor']['link'] ?? '#'); ?>"
+                        target="_blank"><img src="<?php echo MPRW_PLUGIN_URL . "assets/tripadvisor.svg" ?>"
+                            alt="Tripadvisor" width="30px" height="30px"></a></li>
+                <li><a href="<?php echo esc_url($options['platforms']['trustpilot']['link'] ?? '#'); ?>"
+                        target="_blank"><img src="<?php echo MPRW_PLUGIN_URL . "assets/trustpilot.svg" ?>" alt="Trustpilot"
+                            width="30px" height="30px"></a></li>
             </ul>
         </div>
     </div>
